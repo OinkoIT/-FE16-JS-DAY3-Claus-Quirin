c4218d0591216d52c1bf55d8a761dc1768b8ca94
OinkoIT <claus.mersch@gmail.com>
2022-05-31 11:29:40 +0200

    intermediate

---
+++ claus/02_intermediate/script.js
@@ -0,0 +1,34 @@
+/* ex 1 | Compare two numbers
+
+
+Create a JavaScript program that will show the average grade for each student. Here are the points for each student:
+
+Martin = 76
+
+Thomas = 85
+
+Klaus = 65
+
+Maria = 93
+
+David = 81
+
+Based on their points, output the grade, according to the following table:
+
+< 60 F
+
+< 70 D
+
+< 80 C
+
+< 90 B
+
+< 100 A
+
+Finally, calculate the class average and output it (e.g. if the class average is 74 points, their average grade will be C). */
+
+var studentPoints = [["Martin", "Thomas", "Klaus", "Maria", "David"], [76, 85, 65, 93, 81]];
+
+console.log(studentPoints[1][0]);
+
+if
