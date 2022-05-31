2a8bea4c934fc22afafb28c39fcc321b652656da
OinkoIT <claus.mersch@gmail.com>
2022-05-31 12:26:12 +0200

    finaly finish int

--- claus/02_intermediate/script.js
+++ claus/02_intermediate/script.js
@@ -86,14 +86,36 @@ Fizz
 FizzBuzz
 etc. */
 
-for (i=1; i<=100; i++) {
-    if (i%3==0 && i%5==0) {
-        console.log("FizzBuzz")
-    } else if ( i%3==0) {
-        console.log("Fizz")
-    } else if ( i%5==0) {
-        console.log("Buzz")
-    } else {
-        console.log(i)
-    }
+// for (i=1; i<=100; i++) {
+//     if (i%3==0 && i%5==0) {
+//         console.log("FizzBuzz")
+//     } else if ( i%3==0) {
+//         console.log("Fizz")
+//     } else if ( i%5==0) {
+//         console.log("Buzz")
+//     } else {
+//         console.log(i)
+//     }
+// }
+
+/* ex 3
+try to recreate this form using a loop :
+
+*
+
+**
+
+***
+
+****
+
+*****
+
+****** */
+
+var asterix = []
+
+for (i=1; i<=6; i++) {
+    asterix.push("*");
+    console.log(asterix);
 }
